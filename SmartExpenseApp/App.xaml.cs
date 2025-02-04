ec7ea9959719363ec59c17aa2420942170d8eac9
Piyush Jogale <jogalepiyush4580@outlook.com>
2025-02-04 09:43:26 +0530

    Created a signup page

--- SmartExpenseApp/App.xaml.cs
+++ SmartExpenseApp/App.xaml.cs
@@ -11,7 +11,7 @@ namespace SmartExpenseApp
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
-            return new Window(new AddAccount());
+            return new Window(new SignUp());
         }
     }
 }
---
+++ SmartExpenseApp/Views/SignUp.xaml.cs
@@ -0,0 +1,9 @@
+namespace SmartExpenseApp.Views;
+
+public partial class SignUp : ContentPage
+{
+	public SignUp()
+	{
+		InitializeComponent();
+	}
+}
