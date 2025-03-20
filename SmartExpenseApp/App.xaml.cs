a43a21297b726e7b6600e7c20fd5cbf5059c7cdc
Purv Gandhi <gandhipurv2006@gmail.com>
2025-03-20 17:20:30 +0530

    Added Transaction Details Screen

--- SmartExpenseApp/App.xaml.cs
+++ SmartExpenseApp/App.xaml.cs
@@ -11,7 +11,7 @@ namespace SmartExpenseApp
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
-            return new Window(new TransactionDetails());
+            return new Window(new AppShell());
         }
     }
 }
